b6fe9341700d3e5cc9389642fa9bde6c1e83eda2
caylemweeks <74592359+caylemweeks@users.noreply.github.com>
2020-11-17 23:19:07 +0800

    Version 1.0.2 released!
    This version adds the ability to add custom screenshot destinations. Simply install and run; it SHOULD ask you where you want your files to go. If you get errors, it's likely that your folder structure isn't right.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+//Dependancies
 const captureWebsite = require('capture-website');
 const prompt = require('prompt-sync')();
- 
-const URL = prompt('What is the URL?');
-const NAME = prompt('What is the filename?') + '.png';
+const fs = require('fs');
+var path = require("path");
 
+//Checking for valid path.
+var pathdata = fs.readFileSync(path.resolve(__dirname, "path.txt")).toString();
+console.log(pathdata);
+if (pathdata == '' || pathdata == 'firsttime') {
+	var pathdata = prompt('Where do you want your screenshots to end up?');
+	const data = fs.writeFileSync(path.resolve(__dirname, "path.txt"), pathdata);
+}
+
+//Grabbing user input for website capture.
+var URL = prompt('What is the URL?');
+var NAME = prompt('What is the filename?') + '.png';
+
+//Actual website capturing.
 (async () => {
-	await captureWebsite.file(String(URL), NAME, {fullPage: 'True'}), page.setDefaultNavigationTimeout(0);
+	await captureWebsite.file(String(URL), String(pathdata) + '/' + String(NAME), {fullPage: 'True'}), page.setDefaultNavigationTimeout(0);
 })();
